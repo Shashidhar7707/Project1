1fc475fc833778fdde06edda01f2314d922f1deb
shashidhar7707 <shashidharreddym9@gmail.com>
2020-06-16 23:31:58 -0400

    initial commit

---
+++ src/app/componentone/componentone.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+// import { HEROES } from '../mock-heroes'; //(since we are importing HeroService that has HEROES)
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-componentone',
+  templateUrl: './componentone.component.html',
+  styleUrls: ['./componentone.component.css']
+})
+export class ComponentoneComponent implements OnInit {
+  // heroes = HEROES;
+  heroes : Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();  // it is a function to retrieve heroes from the service
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }                                                                  
+
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
